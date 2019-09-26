d9721f491e4a41a3792c5f11efbb73f7acb0035f
simonyagas <simonyagas@gmail.com>
2019-09-26 18:50:44 -0300

    nuevo carrusel piola

--- index.php
+++ index.php
@@ -35,32 +35,32 @@ require_once('helpers.php');
 	    ?>
     <!-- Menu de navegacion -->
 
-     <!-- Carrusel para pc -->
-     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-       <div class="carousel-inner">
-         <div class="carousel-item active">
-           <img class="d-block w-100 _carrusel img-fluid" src="img/carousel1.webp" alt="First slide">
-         </div>
-         <div class="carousel-item">
-           <img class="d-block w-100 _carrusel img-fluid" src="img/carousel2.webp" alt="Second slide">
-         </div>
-         <div class="carousel-item">
-           <img class="d-block w-100 _carrusel img-fluid" src="img/carousel3.webp" alt="Third slide">
-         </div>
-       </div>
-       <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-         <span class="sr-only">Previous</span>
-       </a>
-       <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-         <span class="sr-only">Next</span>
-       </a>
-     </div>
-     <div id="fijaMobile">
-       <img class="chiquita" src="./img/carousel1Mobile.webp" alt="">
+  <!-- Carrusel para pc -->
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100 _carrusel img-fluid" src="img/carousel-tocada-2.jpg" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100 _carrusel img-fluid" src="img/carousel-tocada-3.jpg" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100 _carrusel img-fluid" src="http://placehold.it/1345x500" alt="Third slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+  <div id="fijaMobile">
+    <img class="chiquita" src="./img/carousel-tocada-2.jpg" alt="">
 
-     </div>
+  </div>
 
 <!-- Footer -->
 <?php
